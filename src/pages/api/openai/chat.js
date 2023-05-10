06c4d99827e0396c7454237688e66bcc25f49695
yhsssss <yhsg2e@gmail.com>
2023-05-10 18:12:04 +0900

    :zap: axios 요청 수정

--- src/pages/api/openai/chat.js
+++ src/pages/api/openai/chat.js
@@ -1,16 +1,17 @@
 import { Configuration, OpenAIApi } from "openai";
 const configuration = new Configuration({
-    organization: "org-b5JAEhnrXbyCjozsu6QuOI5H",
-    apiKey: "",
+  organization: "org-b5JAEhnrXbyCjozsu6QuOI5H",
+  apiKey: "",
 });
 const openai = new OpenAIApi(configuration);
 
-export default async function handler(req, res)  {
-    const context = req.body
-    var query =  req.body.query
-    const response = await openai.createChatCompletion({
-        "model": "gpt-3.5-turbo",
-        "messages": [{"role": "user", "content": query}]
-    })
-    res.status(200).json({ answer: response.data.choices[0].message })
+export default async function handler(req, res) {
+  const context = req.body;
+  var query = req.body.query;
+  const response = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [{ role: "user", content: query }],
+  });
+  console.log("response : " + response.data.answer.choices[0].message);
+  res.status(200).json({ answer: response.data.answer.choices[0].message });
 }
--- src/pages/chat.js
+++ src/pages/chat.js
@@ -1,45 +1,57 @@
-import React, {useState, useEffect} from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 export default function Chat() {
-  const [query, setQuery] = useState('');
-  const [answer, setAnswer] = useState('안녕하세요?');
+  const [query, setQuery] = useState("");
+  const [answer, setAnswer] = useState("안녕하세요?");
 
   const onChange = (event) => {
-    setQuery(event.target.value)
-  }
+    console.log("query : " + query);
+    setQuery(event.target.value);
+  };
 
-  const send = async () => { 
-    console.log(query)
-    const result = await axios.post('/api/openai/chat', query)
-    console.log(result.data)
-    setAnswer(result.data)
-  }
+  const send = async () => {
+    console.log(query);
+    const result = await axios
+      .post("/api/openai/chat", {
+        query: query,
+      })
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+    console.log(result);
+    //setAnswer(result.data);
+  };
 
   return (
-        
     <div className="color">
       <div className="pt-24">
         <div className="container px-3 mx-auto flex flex-wrap flex-col md:flex-row items-center">
-          
-            <div className="flex flex-col w-full md:w-3/5 justify-center items-start text-center md:text-left">
-                <p className="uppercase tracking-loose w-full">Ask Anything!</p>
-                <h1 className="my-4 text-5xl font-bold leading-tight">
-                  Main Hero Message to sell yourself!
-                </h1>
-                <div className="leading-normal text-2xl mb-8">
-                  <input onChange={onChange}class="w-full border-2 border-indigo-500" name="query"></input>
-                  <button onClick={send} className="mx-auto lg:mx-0 hover:underline bg-indigo-500 text-white font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
-                  send
-                  </button>
-                </div>
-                <div>
-              </div>
+          <div className="flex flex-col w-full md:w-3/5 justify-center items-start text-center md:text-left">
+            <p className="uppercase tracking-loose w-full">Ask Anything!</p>
+            <h1 className="my-4 text-5xl font-bold leading-tight">
+              Try to send some message to GPT!
+            </h1>
+            <div className="leading-normal text-2xl mb-8">
+              <input
+                onChange={onChange}
+                className="w-full border-2 border-indigo-500"
+                name="query"
+              ></input>
+              <button
+                onClick={send}
+                className="mx-auto lg:mx-0 hover:underline bg-indigo-500 text-white font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out"
+              >
+                Send
+              </button>
             </div>
-            <div className="flex">
-                {answer}
-            </div>
+            <div></div>
+          </div>
+          <div className="flex">{answer}</div>
         </div>
       </div>
     </div>
-    )
-  }
+  );
+}
